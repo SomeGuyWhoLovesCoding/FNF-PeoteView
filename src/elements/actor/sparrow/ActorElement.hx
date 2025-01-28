7f375e5c187c9c089e128eb4e747e08a118e2f55
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-01-28 14:00:05 -0500

    Fix for @jobf again (but for his laptop)

--- src/elements/actor/sparrow/ActorElement.hx
+++ src/elements/actor/sparrow/ActorElement.hx
@@ -25,21 +25,21 @@ class ActorElement implements Element {
 	@texSizeX private var clipSizeX:Int = 1;
 	@texSizeY private var clipSizeY:Int = 1;
 
-	@varying @custom @formula("_mirror == 1 ? (_flipX == 0 ? 1 : 0) : _flipX") var _flipX:Int = 0;
-	@varying @custom var _flipY:Int = 0;
-	@varying @custom var _mirror:Int = 0;
+	@varying @custom @formula("_mirror == 1.0 ? (_flipX == 0.0 ? 1.0 : 0.0) : _flipX") var _flipX:Float = 0.0;
+	@varying @custom var _flipY:Float = 0.0;
+	@varying @custom var _mirror:Float = 0.0;
 
 	var flipX(default, set):Bool;
 
 	inline function set_flipX(value:Bool):Bool {
-		_flipX = value ? 1 : 0;
+		_flipX = value ? 1.0 : 0.0;
 		return flipX = value;
 	}
 
 	var flipY(default, set):Bool;
 
 	inline function set_flipY(value:Bool):Bool {
-		_flipY = value ? 1 : 0;
+		_flipY = value ? 1.0 : 0.0;
 		return flipY = value;
 	}
 
@@ -50,13 +50,13 @@ class ActorElement implements Element {
 		return mirror = value;
 	}
 
-	@posX @formula("x + off_x + px + adjust_x + (w * (_mirror == 1 ? _flipX : -_flipX))") var x:Float;
+	@posX @formula("x + off_x + px + adjust_x + (w * (_mirror == 1.0 ? _flipX : -_flipX))") var x:Float;
 	@posY @formula("y + off_y + py + adjust_y + (h * _flipY)") var y:Float;
-	@sizeX @formula("(w * scale) * (_flipX == 1 ? -1.0 : 1.0)") var w:Float;
-	@sizeY @formula("(h * scale) * (_flipY == 1 ? -1.0 : 1.0)") var h:Float;
+	@sizeX @formula("(w * scale) * (_flipX == 1.0 ? -1.0 : 1.0)") var w:Float;
+	@sizeY @formula("(h * scale) * (_flipY == 1.0 ? -1.0 : 1.0)") var h:Float;
 
-	@pivotX @formula("(w < 0 ? -w : w) * 0.5") var px:Float;
-	@pivotY @formula("(h < 0 ? -h : h) * 0.5") var py:Float;
+	@pivotX @formula("(w < 0.0 ? -w : w) * 0.5") var px:Float;
+	@pivotY @formula("(h < 0.0 ? -h : h) * 0.5") var py:Float;
 
 	@rotation var r:Float;
 
