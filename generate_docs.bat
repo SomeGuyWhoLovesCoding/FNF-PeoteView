0868b47a14f6ffc98fa2ebd74d59a35a12df19f2
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-02-11 18:34:17 -0500

    Update generate_docs.bat

--- generate_docs.bat
+++ generate_docs.bat
@@ -6,7 +6,7 @@ timeout /t 1 /nobreak >nul
 haxelib install dox
 haxelib install hxargs
 lime build windows -xml -D FV_BIG_BYTES
-haxelib run dox -o bin/api -i bin/windows/types.xml -in system -in elements -in utils -in music
+haxelib run dox -o bin/api -i bin/windows/types.xml -in system -in elements -in utils -in music -in structures -in data
 cd ./bin/api
 index.html
 
