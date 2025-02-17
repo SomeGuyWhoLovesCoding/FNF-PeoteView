3d355ce8cd933c393fdb7c43415df9b58842424a
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-02-17 11:34:30 -0500

    Fix `resetNotes`
    Thank FUCKING god

--- src/structures/gameplay/NoteSpawner.hx
+++ src/structures/gameplay/NoteSpawner.hx
@@ -100,10 +100,45 @@ class NoteSpawner {
 
 		if (pf.disposed || pf.died) return;
 
-		parent.resetStrumlines();
-
 		parent.notesHit.clear();
 		parent.notesMissed.clear();
 		parent.notesHeld.clear();
+
+		var file = pf.chart.file;
+		var len = file.length;
+
+		var incrementAmount = (len / 100) * 25;
+		var decrementAmount = (len / 100) * 12;
+
+		var songPos = Tools.betterInt64FromFloat(songPosition * 100);
+		var songPosTop = songPos + spawnDist;
+
+		if (file.getNote(0).position > songPosTop || file.getNote(0).position > songPos) {
+			top = bottom = 0;
+			curBottomNote = curTopNote = file.getNote(0);
+			parent.resetStrumlines();
+			return;
+		}
+
+		var lenSub1 = len - 1;
+
+		while (file.getNote(top).position < songPosTop) {
+			if ((top += incrementAmount) > lenSub1) top = lenSub1;
+		}
+		while (file.getNote(top--).position > songPosTop) {}
+
+		bottom = top;
+
+		while (file.getNote(bottom).position > songPos) {
+			if ((bottom -= decrementAmount) < zero) bottom = zero;
+		}
+		while (file.getNote(bottom++).position < songPos) {}
+
+		curBottomNote = file.getNote(bottom);
+		curTopNote = file.getNote(top);
+
+		parent.resetStrumlines();
 	}
+
+	private var zero(default, null):Int64 = 0;
 }
