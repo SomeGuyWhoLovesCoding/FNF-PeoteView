fb5c12e5035a2b8ac95b8739967b8ff76e5cb291
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-03-24 21:06:00 -0400

    Update CustomDisplay.hx

--- src/elements/CustomDisplay.hx
+++ src/elements/CustomDisplay.hx
@@ -6,10 +6,6 @@ package elements;
 **/
 @:publicFields
 class CustomDisplay extends Display {
-	var fbDisplay(default, null):Display;
-	var fbBuffer(default, null):Buffer<Sprite>;
-	var fbProgram(default, null):Program;
-
 	var scroll(default, null):Point = {x: 0, y: 0};
 
 	var scale(default, set):Float = 1;
@@ -37,20 +33,7 @@ class CustomDisplay extends Display {
 	function new(x:Int, y:Int, w:Int, h:Int, c:Color) {
 		super(x, y, w, h, c);
 
-		fbDisplay = new Display(x, y, w, h, c);
-		Main.current.peoteView.addFramebufferDisplay(fbDisplay);
-		fbDisplay.setFramebuffer(new Texture(w, h));
-
-		fbBuffer = new Buffer<Sprite>(1);
-		fbProgram = new Program(fbBuffer);
-		fbProgram.setTexture(fbDisplay.fbTexture);
-		fbDisplay.addProgram(fbProgram);
-
-		var elem = new Sprite();
-		elem.w = w;
-		elem.h = h;
-		elem.x = 60;
-		fbBuffer.addElement(elem);
+		scroll.update = update;
 	}
 
 	function update() {
@@ -60,9 +43,7 @@ class CustomDisplay extends Display {
 	}
 
 	function renderFB() {
-		var elem = fbBuffer.getElement(0);
-		elem.r += 0.01;
-		fbBuffer.updateElement(elem);
+		
 	}
 
 	function shake(x:Float, y:Float) {
