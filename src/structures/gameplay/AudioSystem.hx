a3b4aa4ce3cd73fe11c3132ead8446eff5701836
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-07-04 11:57:43 -0400

    The fucks are found and gone

--- src/structures/gameplay/AudioSystem.hx
+++ src/structures/gameplay/AudioSystem.hx
@@ -1,12 +1,11 @@
 package structures.gameplay;
 
-import Miniaudio.MaSoundGroup;
 import Miniaudio.MaResult;
 
 /**
 	The auditory system for the playfield.
 	This is an internal structure and should only be used inside of the playfield NOT to be touched with.
-	Please hope to god the fucking miniaudio mixing shit can actually be written in the future.
+	Please hope to god the fucking miniaudio mixing shit can actually be implemented in the future.
 	E.g. the developer's been having this issue that all the sounds' timing don't match up at the exact same moment when you change the time of an audio VERY frequently or when it happens due to minor lag from background processes.
 	I swear, please make this end.
 	@since Development
@@ -17,8 +16,6 @@ class AudioSystem {
 	var inst:Sound;
 	var voices:Array<Sound> = [];
 
-	var soundgrp:MaSoundGroup;
-
 	function new(chart:Chart) {
 		inst = new Sound();
 		inst.fromFile(chart.header.instDir);
@@ -28,29 +25,14 @@ class AudioSystem {
 			voicesInstance.fromFile(voicesDir);
 			voices.push(voicesInstance);
 		}
-
-		var result = Miniaudio.ma_sound_group_init(Sound.engine, 0, null, soundgrp);
-
-		if (result != MaResult.MA_SUCCESS) {
-			Sys.println("[Sound system] Failed to initialize engine");
-			return;
-		} else {
-			Sys.println("HHAAAAHHHH");
-		}
 	}
 
 	function play() {
-		Miniaudio.ma_sound_group_set_pitch(soundgrp, 1);
-		Miniaudio.ma_sound_group_set_volume(soundgrp, 1);
-
 		inst.play();
 
 		for (voicesTrack in voices) {
 			voicesTrack.play();
 		}
-
-		Miniaudio.ma_sound_group_stop(soundgrp);
-		Miniaudio.ma_sound_group_start(soundgrp);
 	}
 
 	function stop() {
@@ -75,21 +57,10 @@ class AudioSystem {
 	}
 
 	function setTime(time:Float) {
-		var timeInSec:cpp.Float64 = time * 0.001;
-		var sampleRate:cpp.UInt32 = 48000;
+		inst.time = time;
 
-		var dataSource = Miniaudio.ma_sound_get_data_source(untyped cast soundgrp);
-		Miniaudio.ma_data_source_get_data_format(dataSource, null, null, cpp.Pointer.addressOf(sampleRate).ptr, null, 0);
-
-		Miniaudio.ma_sound_seek_to_pcm_frame(untyped cast soundgrp, untyped (sampleRate * timeInSec));
-
-		@:privateAccess {
-			var programPos = -Timestamp.get() + timeInSec;
-			inst._programPos = programPos;
-
-			for (voicesTrack in voices) {
-				voicesTrack._programPos = programPos;
-			}
+		for (voicesTrack in voices) {
+			voicesTrack.time = time;
 		}
 	}
 
@@ -101,7 +72,5 @@ class AudioSystem {
 		}
 		voices.resize(0);
 		voices = null;
-
-		Miniaudio.ma_sound_group_uninit(soundgrp);
 	}
 }
