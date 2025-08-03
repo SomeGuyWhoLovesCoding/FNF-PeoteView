f4231b3837e7cbfaad89905555f00309e946e037
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-08-03 16:04:04 -0400

    Revert "Delete setup-linux.bash"
    This reverts commit 51fb492dda49a226b3b0a8c39d391871ee45a58d.

---
+++ setup-linux.bash
@@ -0,0 +1,13 @@
+git clone --recursive https://github.com/HaxeFoundation/haxe.git
+git checkout 4.3.7
+sudo apt install libpcre2-dev zlib1g-dev libmbedtls-dev opam neko
+sudo apt install build-essential
+sudo apt install libx11-dev
+sudo apt install libxinerama-dev
+sudo apt install libxrandr-dev
+sudo apt install mesa-common-dev
+sudo apt install libglu1-mesa-dev
+sudo apt install libxi-dev
+sudo apt install libasound2-dev
+sudo apt install opam
+opam init
