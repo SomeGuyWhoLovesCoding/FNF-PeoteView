493b3c7ff49b8e20df382ef53a330d3888fe1ae6
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-08-03 18:02:30 -0400

    Update setup-linux.bash

--- setup-linux.bash
+++ setup-linux.bash
@@ -7,7 +7,4 @@ sudo apt install libglu1-mesa-dev
 sudo apt install libxi-dev
 sudo apt install libasound2-dev
 sudo apt install git
-cd "/home/$USER/haxe-manager-master"
-git clone --recursive https://github.com/kLabz/haxe-manager.git
-export PATH "/home/$USER/haxe-manager-master/bin/$PATH"
-export HAXE_STD_PATH "/home/$USER/haxe-manager-master/bin/std"
+sudo apt install g++-multilib libc6-dev-i386
