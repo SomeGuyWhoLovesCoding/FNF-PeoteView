ea468b5c8dd1529a03f4253e89b6935e901873e8
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-08-03 16:17:11 -0400

    Update setup-linux.bash

--- setup-linux.bash
+++ setup-linux.bash
@@ -1,6 +1,3 @@
-git clone --recursive https://github.com/HaxeFoundation/haxe.git
-git checkout 4.3.7
-sudo apt install libpcre2-dev zlib1g-dev libmbedtls-dev opam neko
 sudo apt install build-essential
 sudo apt install libx11-dev
 sudo apt install libxinerama-dev
@@ -9,5 +6,8 @@ sudo apt install mesa-common-dev
 sudo apt install libglu1-mesa-dev
 sudo apt install libxi-dev
 sudo apt install libasound2-dev
-sudo apt install opam
-opam init
+sudo apt install git
+cd "/home/$USER/haxe-manager-master"
+git clone --recursive https://github.com/kLabz/haxe-manager.git
+export PATH "/home/$USER/haxe-manager-master/bin/$PATH"
+export HAXE_STD_PATH "/home/$USER/haxe-manager-master/bin/std"
