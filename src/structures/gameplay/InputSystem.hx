78e37cddabf0693ce67462756e3c03408818edfc
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-08-15 12:14:22 -0400

    Update InputSystem.hx

--- src/structures/gameplay/InputSystem.hx
+++ src/structures/gameplay/InputSystem.hx
@@ -187,7 +187,7 @@ class InputSystem {
 
 		if (noteSystem != null) {
 			var strumline = noteSystem.strumlines[lane];
-			if (!strumline.playerHitsToCheck[index] {
+			if (!strumline.playerHitsToCheck[index]) {
 				strumline.playerHitsToCheck[index] = true;
 				strumline.press(index);
 			}
