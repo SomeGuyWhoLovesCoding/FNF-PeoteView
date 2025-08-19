694909d8cf498f254988e21c2b17773ad646a622
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-08-19 09:22:10 -0400

    Preinstalled hl 1.15

--- src/custom/haxe/MetaNoteMap.hx
+++ src/custom/haxe/MetaNoteMap.hx
@@ -1,14 +1,15 @@
 package custom.haxe;
 
 /**
- * This map implementation was made because you can't initialize a normal `haxe.ds.Map<MetaNote, V>` on the hashlink target.
+ * This map implementation was made with a varying underlying type because you can't initialize a normal `haxe.ds.Map<MetaNote, V>` on the hashlink target.
  */
-@:multiType
-abstract MetaNoteMap<T>(Map<MetaNote, T>) {
+abstract MetaNoteMap<T>(#if cpp Map<MetaNote, T> #else hl.types.Int64Map #end) {
 	/**
 		Creates a new MetaNoteMap.
 	**/
-	public function new():Void;
+	inline public function new():Void {
+		this = new #if cpp Map<MetaNote, T> #else hl.types.Int64Map #end ();
+	};
 
 	/**
 		See `Map.set`
