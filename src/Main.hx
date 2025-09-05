1fc2469d3e8763c8d92426d24acaaa6799f480c0
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-09-04 21:15:46 -0400

    It even works on termination too

--- src/Main.hx
+++ src/Main.hx
@@ -57,12 +57,12 @@ class Main extends Application
 			// START CHART SAMPLE
 			var stamp = haxe.Timer.stamp();
 			trace("Insert 1,000,000 notes (array)");
-			Chart.load("assets/songs/god-eater");
+			Chart.load("assets/songs/termination");
 			var arr = new Array<MetaNote>();
 			for (i in 0...1000000) {
 				arr.push(new MetaNote(Tools.betterInt64FromFloat((50.0 + (50.0 * i)) * 100),
 					Math.floor(/*100*/0 * 0.2), // Equal to `note.duration / 5`.
-					i % 9,
+					i % 4,
 					0,
 				1));
 				//Sys.println(i);
