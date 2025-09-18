154dca3cf811389bdd045d1d711101ead0eca4d0
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-09-18 17:41:38 -0400

    Revert "I'm undoing this"
    This reverts commit f873381b54a803b20f7e435f706cbfb23ce2d832.

--- src/data/chart/chart_file.cpp
+++ src/data/chart/chart_file.cpp
@@ -14,8 +14,6 @@
 #include <unistd.h>
 #endif
 
-#include "DiskMultiMap.h"
-
 // ============================================================================
 // MappedFile class - encapsulates mmap / CreateFileMapping
 // ============================================================================
@@ -115,12 +113,14 @@ MappedFile gFile;
 // ============================================================================
 // Deferred Inserts and Removals optimization technique (unfinished)
 // ============================================================================
-DiskMultiMap inserts;
-DiskMultiMap removals;
+MappedFile inserts;
+MappedFile removals;
 
 void insertDeferred(int64_t note) {
-    int64_t index = 0;
-    inserts.insert(index,note); // key, value, context (additional info)
+    int64_t size = inserts.size();
+    inserts.resize(size + 1);
+    inserts.raw()[size << 1] = note;
+    inserts.raw()[(size + 1) << 1] = note;
 }
 
 void removeDeferred(int64_t index) {
@@ -132,11 +132,6 @@ void removeDeferred(int64_t index) {
 
 int64_t* __restrict data = nullptr;
 int64_t length = 0;
-int64_t indexOffsetPos = 0; // For sequential `getNote(atIndex)`. Resets when updating the 
-
-void resetGetNoteLookup() {
-    indexOffsetPos = 0;
-}
 
 bool remap(size_t newLength) {
     bool ok = gFile.resize(newLength);
@@ -150,10 +145,19 @@ void loadChart(const char* inFile) {
         throw std::runtime_error("Failed to open chart file");
 
     std::string insertsPath = std::string(inFile) + "_deferredInserts.bin";
-    inserts.createNew(insertsPath, 10000); // 10000 is the number of buckets
+    if (!inserts.open(insertsPath.c_str())) {
+        // File probably doesn't exist or is empty: create 1-element file
+        inserts.open(insertsPath.c_str());
+        inserts.resize(1024 * 16);
+    }
+    inserts.resize(1024 * 16);
 
     std::string removalsPath = std::string(inFile) + "_deferredRemoves.bin";
-    inserts.createNew(removalsPath, 10000); // 10000 is the number of buckets
+    if (!removals.open(removalsPath.c_str())) {
+        removals.open(removalsPath.c_str());
+        removals.resize(1024 * 16);
+    }
+    removals.resize(1024 * 16);
 
     // safe: data can be nullptr if file is empty
     data = gFile.raw();
