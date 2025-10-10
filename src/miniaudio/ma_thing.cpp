56b02e81f873642ae7d39a260270945dcd4c1a0d
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-10-10 19:01:17 -0400

    m

--- src/miniaudio/ma_thing.cpp
+++ src/miniaudio/ma_thing.cpp
@@ -41,7 +41,7 @@ int g_measuredLatencyMs = 0; // cached loopback latency
 // -------------------- LOOPBACK LATENCY MEASUREMENT --------------------
 int detectLatency() {
 	int bufferLatency = 10; // simple.
-	int osMs = 100; // can be VERY important. this is an approximate of windows audio/video latency in total
+	int osMs = 80; // can be VERY important. this is an approximate of windows audio/video latency in total
 	int result = bufferLatency + osMs + g_measuredLatencyMs;
 
     if (g_measuredLatencyMs != 0) return result;
