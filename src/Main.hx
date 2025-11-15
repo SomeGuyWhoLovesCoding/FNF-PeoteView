0ac0eb70d1c0e976d8e6de903a272d06e7baff66
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-11-14 21:33:10 -0500

    Update Main.hx

--- src/Main.hx
+++ src/Main.hx
@@ -414,7 +414,7 @@ class Main extends Application
 					var scoreTxt = HUD.scoreTxt;
 					var noteSpawner = noteSystem.noteSpawner;
 					//if (scoreTxt != null) scoreTxt.text = ((noteSpawner.timeSpentOnIt * 1000000000) / Tools.int64ToFloat(noteSpawner.top - noteSpawner.bottom)) + "ns";
-					if (scoreTxt != null) scoreTxt.text = (noteSpawner.timeSpentOnIt * 1000) + "ms";
+					//if (scoreTxt != null) scoreTxt.text = (noteSpawner.timeSpentOnIt * 1000) + "ms";
 
 					hud.updateBuffers();
 				}
