20fcee6b21b1fb7b2a524e97943ffa3d45812307
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-11-18 19:01:23 -0500

    Update NoteSystem.hx

--- src/structures/gameplay/NoteSystem.hx
+++ src/structures/gameplay/NoteSystem.hx
@@ -246,7 +246,6 @@ class NoteSystem {
 						noteTypeCall(index, type, true);
 					}
 
-					Sys.println(@:privateAccess parent.onNoteMiss.__listeners.length != 0);
 					if (@:privateAccess parent.onNoteMiss.__listeners.length != 0)
 						parent.onNoteMiss.dispatch(note, noteSpr.notesInOne);
 					if (parent.field != null)
