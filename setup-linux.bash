e13f22cc91827e497ce556baeeaf2beb370626e9
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-11-26 13:56:54 -0500

    v

--- setup-linux.bash
+++ setup-linux.bash
@@ -1,3 +1,4 @@
+# this is needed for installing libdrm-dev to work
 sudo sed -i 's/security.ubuntu.com/archive.ubuntu.com/g' /etc/apt/sources.list
 sudo apt-get update
 
@@ -12,7 +13,7 @@ sudo apt-get install libxinerama-dev
 sudo apt-get install libgl-dev
 sudo apt-get install libgl1-mesa-dev
 sudo apt-get install libasound2-dev
-sudo apt-get install libdrm-dev
+sudo apt-get install libdrm2 libdrm-common libdrm-dev
 
 # This shit is fucking important
 sudo dpkg --add-architecture i386
@@ -20,7 +21,7 @@ sudo apt update
 sudo apt-get install libgl-dev:i386
 sudo apt-get install libgl1-mesa-dev:i386
 sudo apt-get install libglu1-mesa-dev:i386
-sudo apt-get install libdrm-dev:i386
+sudo apt-get install libdrm2:i386 libdrm-common libdrm-dev:i386
 
 # Now onto the shit we actually want to do on our end
 haxelib setup ~/haxelib
