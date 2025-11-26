c5ed64f7b9fcf165cae31b4ba1d7d2ae39c8f28d
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-11-26 13:42:56 -0500

    Who could've guessed

--- setup-linux.bash
+++ setup-linux.bash
@@ -9,6 +9,7 @@ sudo apt-get install libxinerama-dev
 sudo apt-get install libgl-dev
 sudo apt-get install libgl1-mesa-dev
 sudo apt-get install libasound2-dev
+sudo apt-get install libdrm-dev
 
 # This shit is fucking important
 sudo dpkg --add-architecture i386
@@ -16,6 +17,7 @@ sudo apt update
 sudo apt-get install libgl-dev:i386
 sudo apt-get install libgl1-mesa-dev:i386
 sudo apt-get install libglu1-mesa-dev:i386
+sudo apt-get install libdrm-dev:i386
 
 # Now onto the shit we actually want to do on our end
 haxelib setup ~/haxelib
