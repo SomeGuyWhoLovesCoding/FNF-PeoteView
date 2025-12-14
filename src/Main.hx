4397c6d11a64e105313165be1123a5749dbaa932
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-12-13 20:05:33 -0500

    Fix titlebar font
    Actually get it working cuz according to windows api you have to specify font name to get it to work

--- src/Main.hx
+++ src/Main.hx
@@ -55,7 +55,7 @@ class Main extends Application
 		//Titlebar.setPrimaryButtonImage("assets/system/WM/maximize.png");
 		Titlebar.setButtonFontColor(255, 255, 255);
 		var path = sys.FileSystem.absolutePath('assets/fonts/inconsolata/inconsolata-semibold.ttf');
-		Titlebar.setTitleFont("Inconsolata-SemiBold", 'assets/fonts/inconsolata/inconsolata-semibold.ttf', 16);
+		Titlebar.setTitleFont("Inconsolata SemiBold", 'assets/fonts/inconsolata/inconsolata-semibold.ttf', 18);
 		Titlebar.initialize();
 		Titlebar.redrawWindow();
 		#end
