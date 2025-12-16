e15144a557317c89e911046455ca7975343f34c4
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-12-15 19:42:08 -0500

    Now I just gotta get a cleaner outline first

--- src/elements/text/TextCharSprite.hx
+++ src/elements/text/TextCharSprite.hx
@@ -5,8 +5,8 @@ package elements.text;
 **/
 @:publicFields
 class TextCharSprite implements Element {
-	@posX @formula("x - (os + os)") var x:Float;
-	@posY @formula("y - (os + os)")  var y:Float;
+	@posX @formula("x - (w * (os + os))") var x:Float;
+	@posY @formula("y - (h * (os + os))")  var y:Float;
 	@sizeX @formula("w + (w * (os + os))") var w:Float;
 	@sizeY @formula("h + (h * (os + os))") var h:Float;
 
