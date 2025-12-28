bd394193bf9761a58b02406d0ca1bf1b7d29cf4c
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2025-12-28 14:04:05 -0500

    Update RenderingMode.hx

--- src/ffmpeg/RenderingMode.hx
+++ src/ffmpeg/RenderingMode.hx
@@ -352,7 +352,7 @@ class RenderingMode {
 			// Map buffer for faster access (optional optimization)
 			try {
 				// Use getBufferSubData for compatibility
-				GL.bufferData(pboTarget, frameSize, buffer, GL.STREAM_COPY);
+				GL.getBufferSubData(pboTarget, 0, frameSize, buffer);
 				GL.bindBuffer(pboTarget, null);
 				
 				// Enqueue frame
