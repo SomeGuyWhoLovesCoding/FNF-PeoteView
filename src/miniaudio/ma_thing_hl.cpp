867c7c6705009e7698b7082f9be7a8058b835cfb
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-01-01 17:47:53 -0500

    .

--- src/miniaudio/ma_thing_hl.cpp
+++ src/miniaudio/ma_thing_hl.cpp
@@ -2133,7 +2133,7 @@ HL_PRIM int HL_NAME(detectLatency)(_NO_ARG) {
 	#endif
 	if (g_audioSystem.exists) {
 		if(!HL_NAME(wearingPlugNPlay)()) osMs += 50;
-		if(HL_NAME(wearingHeadphones)()) osMs += 30;
+		if(HL_NAME(wearingHeadphones)()) osMs += 25;
 		osMs -= g_audioSystem.getLatencyMs();
 	}
 	return osMs;
