a56c3f783d900b042f5cbb877d3aa89c6ec92057
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-02-26 14:42:19 -0500

    Man I just cannot figure this shit out
    But at least I updated it some

--- src/structures/gameplay/PlayField.hx
+++ src/structures/gameplay/PlayField.hx
@@ -37,25 +37,25 @@ class PlayField {
 
 	var ratingJudgementList:Array<Judgement> = [
 		[
-			0.33, // target
+			0.24, // target
 			0, // id
 			1, // accuracy
 			400 // score
 		],
 		[
-			0.5,
+			0.36,
 			1,
 			0.8,
 			200
 		],
 		[
-			0.66,
+			0.48,
 			2,
 			0.675,
 			100
 		],
 		[
-			0.80,
+			0.60,
 			3,
 			0.5,
 			50
