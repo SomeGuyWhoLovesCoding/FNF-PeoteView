14acf5a20a861ca6c6fc171aa558d4b909387dfe
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-02-28 12:15:36 -0500

    Update FramebufferDisplay.hx

--- src/elements/FramebufferDisplay.hx
+++ src/elements/FramebufferDisplay.hx
@@ -8,7 +8,10 @@ class FramebufferDisplay extends Display {
     var display:Display;
     var texture:Texture;
     
-    var useFramebuffer:Bool;
+    var useFramebuffer(default, set):Bool;
+    inline function set_useFramebuffer(value:Bool) {
+        return setFramebufferEnabled(value);
+    }
 
 	public function new(x:Int, y:Int, width:Int, height:Int, color:Color = 0x00000000, ?useFramebuffer:Bool = true) {
 		super(x, y, width, height, color);
@@ -75,7 +78,7 @@ class FramebufferDisplay extends Display {
         peoteView.removeDisplay(!useFramebuffer ? display : this);
         peoteView.addDisplay(useFramebuffer ? display : this);
         
-        useFramebuffer = enabled;
+        return useFramebuffer = enabled;
     }
 
     public function addIt(peoteView:PeoteView) {
