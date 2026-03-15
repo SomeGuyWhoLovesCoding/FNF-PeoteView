1fc7fb8e32075c10d145b4c41fc817b57d4c8e0d
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-03-15 10:41:33 -0400

    Refactor TextFormatMarkerPair properties and constructor

--- src/elements/TextFormatMarkerPair.hx
+++ src/elements/TextFormatMarkerPair.hx
@@ -1,15 +1,27 @@
 package elements;
 
+@:allow(elements.TextInternal)
 class TextFormatMarkerPair {
-	public var marker:String;
-	public var color:Color;
-	public var outlineColor:Color;
-	public var outlineSize:Float;
-	
-	public function new(marker:String, color:Color, ?outlineColor:Color = 0x000000FF, ?outlineSize:Float = 0) {
-		this.marker = marker;
-		this.color = color;
+	public var marker(default, set):String;
+	public var color(default, set):Color;
+	public var outlineColor(default, set):Color;
+	public var outlineSize(default, set):Float;
+
+	private var _onChange:Void->Void = null;
+
+	private inline function dirty() {
+		if (_onChange != null) _onChange();
+	}
+
+	function set_marker(v:String):String       { marker       = v; dirty(); return v; }
+	function set_color(v:Color):Color          { color        = v; dirty(); return v; }
+	function set_outlineColor(v:Color):Color   { outlineColor = v; dirty(); return v; }
+	function set_outlineSize(v:Float):Float    { outlineSize  = v; dirty(); return v; }
+
+	public function new(marker:String, color:Color, outlineColor:Color = 0x000000FF, outlineSize:Float = 0) {
+		this.marker       = marker;
+		this.color        = color;
 		this.outlineColor = outlineColor;
-		this.outlineSize = outlineSize;
+		this.outlineSize  = outlineSize;
 	}
 }
