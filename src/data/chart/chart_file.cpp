a0665e9d6e3c81cb6b3d7edf66c7889155363881
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-05-19 21:52:21 -0400

    Update chart_file.cpp

--- src/data/chart/chart_file.cpp
+++ src/data/chart/chart_file.cpp
@@ -20,7 +20,7 @@ int64_t getNote(int64_t index) {
 }
 
 void setNote(int64_t index, int64_t value) {
-    //core_setNote(index, value);
+    core_setNote(index, value);
 }
 
 int64_t getLength() {
@@ -28,40 +28,7 @@ int64_t getLength() {
 }
 
 int64_t getTimeCorrectionForIndex(int64_t index) {
-    if (!gReader) throw std::runtime_error("Chart not loaded");
-    int64_t local = 0;
-    gReader->resolve(index, local);
-    return gReader->correctionTime;
-}
-
-// new shit
-bool isNoteHit(int64_t index) {
-    if (!gReader) throw std::runtime_error("Chart not loaded");
-    return core_isNoteHit(index);
-}
-
-void setNoteHit(int64_t index, bool value) {
-    core_setNoteHit(index, value);
-}
-
-bool isNoteMissed(int64_t index) {
-    return core_isNoteMissed(index);
-}
-
-void setNoteMissed(int64_t index, bool value) {
-    core_setNoteMissed(index, value);
-}
-
-bool isNoteHeld(int64_t index) {
-    return core_isNoteHeld(index);
-}
-
-void setNoteHeld(int64_t index, bool value) {
-    core_setNoteHeld(index, value);
-}
-
-// Clears all judgement states.
-// Call this from Haxe's resetNotes() with the lowerBound seek target.
-void clearJudgement() {
-    core_clearJudgement();
+    if (!gReader) return 0;
+    uint64_t shardId = gReader->findShardForGlobalIndex(index);
+    return shardId * 1000000000LL;
 }
