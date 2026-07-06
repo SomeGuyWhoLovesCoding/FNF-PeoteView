8526bc0e8b26017d811bfca6030e729e23b40a0b
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-07-05 22:59:33 -0400

    Revert "wrote this before the two unused finalizer classes lo"
    This reverts commit 7ca3823505887145c45836fd7922d3d69eca578f.

--- src/Main.hx
+++ src/Main.hx
@@ -74,12 +74,6 @@ class Main extends Application
 
 	override function onWindowCreate()
 	{
-		#if (cpp && !debug)
-		//cpp.vm.Gc.setTargetFreeSpacePercentage(50);
-		//cpp.vm.Gc.setMinimumWorkingMemory(256*1024*1024);
-		//new CustomFinalizer();
-		#end
-
 		var titleBarColor:Color = SaveData.state.graphics.customTitleBarColor;
 
 		#if (windows && customtitlebar)
@@ -430,13 +424,6 @@ class Main extends Application
 				storyMenu.update(newDeltaTime);
 			}
 		}
-
-		/*#if cpp
-		var drained = DeferredFinalizer.drain(256);
-		if (drained != 0) {
-			Sys.println('  [ GC Deferred Finalizer System ] Now it\'s $drained objects destroyed/finalized');
-		}
-		#end*/
 	}
 
 	override function render(context:RenderContext) {
