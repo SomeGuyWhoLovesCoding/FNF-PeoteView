229cc04b1a701b3f34ea2972eef7419f25e83b18
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-07-20 09:24:45 -0400

    Update TextureSystem.hx
    Come to think about it, I did this on my phone cuz I still think the power went out. lol

--- src/system/TextureSystem.hx
+++ src/system/TextureSystem.hx
@@ -10,32 +10,20 @@ import sys.io.File;
 #if !debug
 @:noDebug
 #end
+@:final
 @:publicFields
 class TextureSystem {
 	/**
 		The texture pool.
 	**/
-	static var pool:Map<String, Texture> = [];
-
-	static var noteTex(default, null):Texture;
-	static var sustainTex(default, null):Texture;
-
-	/**
-		The multitexture location map.
-	**/
-	static var multitexLocMap:Map<String, Array<Int>> = [];
+	static var pool:FakeStringMap<Texture> = new FakeStringMap<Texture>();
 
 	/**
 		Get a pre-existing texture from pool.
 		@param key The texture to get from.
 	**/
 	inline static function getTexture(key:String) {
-		var tex = null;
-		switch (key) {
-			case "noteTex": tex = noteTex;
-			case "sustainTex": tex = sustainTex;
-			default: tex = pool[key];
-		}
+		var tex = pool.get(key);
 		return tex;
 	}
 
@@ -45,7 +33,6 @@ class TextureSystem {
 		@param key The texture to get from.
 		@param name The texture's new name.
 	**/
-
 	inline static function setTexture(prgm:CustomProgram, key:String, name:String) {
 		prgm.setTexture(getTexture(key), name, true);
 	}
@@ -56,7 +43,6 @@ class TextureSystem {
 		@param key The texture to get from.
 		@param name The texture's new name.
 	**/
-
 	static function disposeTexture(key:String) {
 		if (!pool.exists(key)) return;
 		var tex = getTexture(key);
@@ -106,6 +92,8 @@ class TextureSystem {
 			}
 		}
 
+		image.dispose(); // turns out I forgot to call this the whole time, but oh well.
+
 		var texture = new Texture(textureData.width, textureData.height, null, {
 			format: textureData.format,
 			powerOfTwo: false,
@@ -114,62 +102,6 @@ class TextureSystem {
 		});
 		texture.setData(textureData);
 
-		if (key == "noteTex") noteTex = texture;
-		else if (key == "sustainTex") sustainTex = texture;
-		else pool[key] = texture;
-	}
-
-	/**
-		Create a tiled texture and put it in the texture pool.
-		This accepts horizontal and/or vertical tiled textures.
-		@param key The texture's key.
-		@param path The texture path.
-	**/
-	static function createTiledTexture(key:String, path:String, tX:Int = 1, tY:Int = 1, disableAntialiasing:Bool = false, premultiply:Bool = false) {
-		if (pool.exists(key)) {
-			return;
-		}
-
-		var currentSaveState = SaveData.state.graphics;
-		var antialiasing = currentSaveState.antialiasing && !disableAntialiasing;
-
-		var image = Image.fromFile(Paths.asset(path));
-
-		// I'm proud of this fix, but it couldn't be better be this:
-		var textureData = !premultiply ? TextureData.fromLimeImage(image) : new TextureData(image.width, image.height, TextureFormat.RGBA);
-		if (premultiply) {
-			textureData.bytes = haxe.io.Bytes.alloc(image.width * image.height * 4);
-			var bytes = image.data.toBytes();
-			for (i in 0...textureData.bytes.length >> 2) {
-				var fullARGB = bytes.getInt32(i << 2);
-
-				var a = (fullARGB >>> 24) & 0xFF;
-				var r = (fullARGB >>> 16) & 0xFF;
-				var g = (fullARGB >>> 8)  & 0xFF;
-				var b = (fullARGB)        & 0xFF;
-
-				// Scale RGB by alpha
-				r = (r * a) >> 8; // divide by 255
-				g = (g * a) >> 8;
-				b = (b * a) >> 8;
-
-				var premul = (a << 24) | (r << 16) | (g << 8) | b;
-				textureData.bytes.setInt32(i << 2, premul);
-			}
-		}
-
-		var texture = new Texture(textureData.width, textureData.height, null, {
-			tilesX: tX,
-			tilesY: tY,
-			format: textureData.format,
-			powerOfTwo: false,
-			smoothExpand: antialiasing,
-			smoothShrink: antialiasing
-		});
-		texture.setData(textureData);
-
-		if (key == "noteTex") noteTex = texture;
-		else if (key == "sustainTex") sustainTex = texture;
-		pool[key] = texture;
+		pool.set(key, texture);
 	}
 }
