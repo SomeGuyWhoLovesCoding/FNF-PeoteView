76132fff2d634d9ff95ce4817d1fa1c2da30e645
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-08-13 14:54:43 -0400

    Revert "mm"
    This reverts commit 6683c1f86bf020d424553875e89adf67c88716e8.

--- src/system/StateMachine.hx
+++ src/system/StateMachine.hx
@@ -135,12 +135,9 @@ class StateMachine {
 		}
 
 		current = state;
-		Sys.println('[ StateMachine ] doSwitch: creating $state');
 		safe(() -> current.create());
-		Sys.println('[ StateMachine ] doSwitch: created');
 		rebind();
 		TextureSystem.processQueue();
-		Sys.println('[ StateMachine ] doSwitch: done');
 	}
 
 	function doPush(state:GameState) {
