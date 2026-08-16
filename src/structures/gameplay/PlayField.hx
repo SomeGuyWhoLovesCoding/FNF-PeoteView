e50a763c60b38716ec39a6a2baa80d8d02b95427
SomeGuyWhoLovesCoding <jeremiah.ostrosky@gmail.com>
2026-08-16 16:30:13 -0400

    Revert "reverting this"
    This reverts commit 05a84b3f2f67ca7a02a4b05d819beb36ff2618de.

--- src/structures/gameplay/PlayField.hx
+++ src/structures/gameplay/PlayField.hx
@@ -210,9 +210,6 @@ class PlayField {
 		if (isBackwards)
 			return;
 
-		if (eventSystem != null)
-			eventSystem.seek(value);
-
 		if (hud != null && SaveData.state.preferences.ratingPopup)
 			hud.hideRatingPopup();
 
